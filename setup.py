45477262a774ca8e3b909f945d2b490912dddf9f
David Strootman <david.strootman@gmail.com>
2020-07-02 11:24:13 +0200

    DAT-3578: Automate publishing to PyPI

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
-from setuptools import setup, find_packages
-from os import path
+import os
 
-# Read the README into a variable
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    readme = f.read()
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name='openapi3-fuzzer',
     packages=find_packages(),
     package_data={'': ['openapi3-fuzzer/openapi3_fuzzer/fuzz/*.txt']},
     include_package_data=True,
-    version='1.2.1',
+    version=os.getenv('TAG_NAME', '0.0.0'),
     license='gpl-3.0',
     description='Openapi3 fuzzer',
-    long_description=readme,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='VolkerWessels Telecom',
-    author_email='opensource@vwt.digital',
+    author='VWT Digital',
+    author_email='support@vwt.digital',
     url='https://github.com/vwt-digital/openapi3-fuzzer/tree/master',
     keywords=['Openapi3', 'fuzzer', 'vwt'],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
     ],
-    install_requires=[
-        'prance==0.17.0'
-    ],
+    install_requires=install_requires,
     python_requires='>=3.6',
 )
