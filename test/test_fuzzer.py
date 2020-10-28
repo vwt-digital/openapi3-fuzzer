aaaaafd7d0d5846e6176754a54c7afc866a2ad5c
David Strootman <david.strootman@gmail.com>
2020-10-28 09:21:30 +0100

    DAT-4189: Add negative unittests and add another path to circumvent ambiguous "1" output

--- test/test_fuzzer.py
+++ test/test_fuzzer.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from openapi3_fuzzer import FuzzIt, do_fuzzing
+from prance import ValidationError
 from flask_testing import TestCase
+
+
+class OtherTest(TestCase):
+    def create_app(self):
+        pass
 
 
 class TestFuzzer(TestCase):
@@ -17,7 +23,22 @@ class TestFuzzer(TestCase):
             'Accept': 'application/json',
             'Authorization': 'Bearer',
         }
-        assert do_fuzzing(self, headers, "test/openapi.yaml") == 1
+        assert do_fuzzing(self, headers, "test/openapi.yaml") == 2
 
     def test_FuzzIt_no_errors(self):
         assert FuzzIt("test/openapi.yaml", 'test', self)
+
+    def test_no_test_client(self):
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer',
+        }
+        new_test_case = OtherTest()
+        self.assertRaises(AttributeError, do_fuzzing, new_test_case, headers, "test/openapi.yaml")
+
+    def test_invalid_spec(self):
+        self.assertRaises(ValidationError, FuzzIt, "test/invalid-openapi.yaml", 'test', self)
+
+    def test_no_paths_spec(self):
+        self.assertRaises(ValidationError, FuzzIt, "test/no-paths-openapi.yaml", 'test', self)
